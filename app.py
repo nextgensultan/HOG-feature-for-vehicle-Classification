b712c01124ba7bec87cbb83aeec85c74b427638b
Ibrahim sultan <ibrahim27sultan@gmail.com>
2025-01-22 17:29:00 +0500

    added requirements

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import torch
-from torchvision import transforms
 from PIL import Image
-import os
+from huggingface_hub import hf_hub_download
 from HOG import HOG
 from Softmax import Softmax
 
 # Set up the title
-st.title("🚀 HOG Feature Extraction and Classification")
+st.title("🚀 HOG with Softmax for classification of Cars,planes,Trains")
 
 # Description and instructions
 st.markdown("""
@@ -31,6 +30,36 @@ uploaded_file = st.file_uploader(
     help="Drag and drop or click to upload an image."
 )
 
+# Model URL and file path
+REPO_URL = "ibrahimsultan/HOG_Softmax_image_classifier"  # Hugging Face repository
+MODEL_FILE = "softmax_HOG.pth"  # Model file name
+
+# Initialize the placeholder for downloading message
+download_message = st.empty()
+
+# Path to download the model
+with download_message.container():
+    st.markdown("""
+        <div style="text-align: center; font-size: 16px; color: #0000ff;">
+            <p>Downloading the model... Please wait.</p>
+        </div>
+    """, unsafe_allow_html=True)
+
+    # Download the model
+    downloaded_model_path = hf_hub_download(
+        repo_id=REPO_URL,
+        filename=MODEL_FILE  # Model file you want to download
+    )
+
+# Remove the downloading message after the model is downloaded
+download_message.empty()
+
+# Load the model after downloading
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = Softmax(inputNeurons=8100, hlayer1=2100, outputs=3).to(device)
+model.load_state_dict(torch.load(downloaded_model_path, map_location=device))
+model.eval()
+
 if uploaded_file is not None:
     # Load the image and convert to grayscale
     image = Image.open(uploaded_file).convert('L')
@@ -39,12 +68,6 @@ if uploaded_file is not None:
     hog = HOG(9, 8, 2)
     features = hog.GetFeatures(image)
     features_tensor = torch.tensor(features, dtype=torch.float32).unsqueeze(0)
-
-    # Load the model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = Softmax(inputNeurons=8100, hlayer1=2100, outputs=3).to(device)
-    model.load_state_dict(torch.load("softmax_HOG.pth", map_location=device))
-    model.eval()
 
     # Make a prediction
     with torch.no_grad():
